be0aac1dff64c2e6164e47bf40ab06d81a0b6df9
Abhishek Chaudhary <abhishekchaudhary0100@gmail.com>
2026-05-06 17:24:16 +0000

    simplify whitenoise storage

--- sales_analytics/settings.py
+++ sales_analytics/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
@@ -177,6 +177,6 @@ LOGGING = {
 
 STORAGES = {
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
