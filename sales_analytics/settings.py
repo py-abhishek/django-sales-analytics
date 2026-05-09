19bf546cffe000727ac4bbc34883f2d8753fbc63
Abhishek Chaudhary <abhishekchaudhary0100@gmail.com>
2026-05-09 06:13:26 +0000

    fixed typo error

--- sales_analytics/settings.py
+++ sales_analytics/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = config('DEBUG', defualt=False, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 # ─── Allowed Hosts ───────────────────────────────────────────
 ALLOWED_HOSTS = [
