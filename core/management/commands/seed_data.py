73ccb54f81eab879d92875d953c454a17781a6c0
Abhishek Chaudhary <abhishekchaudhary0100@gmail.com>
2026-05-09 17:38:49 +0000

    update seed_data cmd

--- core/management/commands/seed_data.py
+++ core/management/commands/seed_data.py
@@ -15,30 +15,30 @@ class Command(BaseCommand):
 
 
     def handle(self, *args, **options):
-        # seed_product_categories.seed_product_categories(self.business)
-        # print('Product categories inserted successfully')
+        seed_product_categories.seed_product_categories(self.business)
+        print('Product categories inserted successfully')
 
 
-        # seed_products.seed_products(self.business, self.user)
-        # print('Products inserted successfully')
+        seed_products.seed_products(self.business, self.user)
+        print('Products inserted successfully')
 
-        # seed_suppliers.seed_suppliers(self.business)
-        # print('Suppliers inserted successfully')
+        seed_suppliers.seed_suppliers(self.business)
+        print('Suppliers inserted successfully')
 
         seed_purchases.seed_purchases(self.business)
         print('Purchases added successfully')
 
-        # seed_customers.seed_customers(self.business)
-        # print('Customers inserted successfully')
+        seed_customers.seed_customers(self.business)
+        print('Customers inserted successfully')
 
-        # seed_sales.seed_sales(self.business, self.user)
-        # print('Sales created successfully')
+        seed_sales.seed_sales(self.business, self.user)
+        print('Sales created successfully')
 
-        # seed_expense_categories.seed_expense_categories(self.business)
-        # print('Expese categories created successfully')
+        seed_expense_categories.seed_expense_categories(self.business)
+        print('Expese categories created successfully')
 
-        # seed_expenses.seed_expenses(self.business, self.user)
-        # print('Expenses created added successfully')
+        seed_expenses.seed_expenses(self.business, self.user)
+        print('Expenses created added successfully')
 
 
 
