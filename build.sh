55006c7e526088459d3eefa67960397a7a38dba0
Abhishek Chaudhary <abhishekchaudhary0100@gmail.com>
2026-06-29 17:36:36 +0000

    seed data cmd added

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-# python manage.py seed_b1_data
-# python manage.py seed_b2_data
+python manage.py seed_b1_data
+python manage.py seed_b2_data
