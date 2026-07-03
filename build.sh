86cefbb3192a782b9d5792891919ebdea2e48d73
Abhishek Chaudhary <abhishekchaudhary0100@gmail.com>
2026-07-03 14:03:17 +0000

    b2 data

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
-python manage.py seed_b1_data
-# python manage.py seed_b2_data
+# pip install -r requirements.txt
+# python manage.py collectstatic --noinput
+# python manage.py migrate
+# python manage.py seed_b1_data
+python manage.py seed_b2_data
